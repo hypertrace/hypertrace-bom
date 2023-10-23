0c3b96f08949fe52b34bf4291db2199bab3af1ef
Aaron Steinfeld <aaron@traceable.ai>
2023-10-23 11:21:56 -0400

    refactor: add more dependencies

--- hypertrace-bom/build.gradle.kts
+++ hypertrace-bom/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     api(libs.hypertrace.grpcutils.context)
     api(libs.hypertrace.grpcutils.client)
     api(libs.hypertrace.grpcutils.rx.client)
+    api(libs.hypertrace.grpcutils.rx.server)
     api(libs.hypertrace.framework.grpc)
     api(libs.hypertrace.framework.http)
     api(libs.hypertrace.framework.spi)
@@ -24,9 +25,17 @@ dependencies {
     api(libs.hypertrace.eventstore)
 
     api(libs.guava)
+    api(libs.google.re2j)
+    api(libs.guice)
+    api(libs.rholder.guava.retrying)
+    api(libs.rxjava3)
+    api(libs.uuidcreator)
     api(libs.typesafe.config)
     api(libs.lombok)
     api(libs.protobuf.java)
     api(libs.protobuf.javautil)
+    api(libs.kafka.clients)
+    api(libs.slf4j2.api)
+    api(libs.log4j.slf4j2.impl)
   }
 }
