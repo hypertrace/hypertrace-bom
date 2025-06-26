e32550c92e045ae31431a036958c348059b4ff24
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2025-06-26 08:37:56 -0400

    chore: update bom with new plugin versions (#66)
    * chore: update bom with new plugin versions
    
    * ci: retrigger ci checks
    
    * ci: enable ci

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,12 @@ pluginManagement {
   repositories {
     mavenLocal()
     gradlePluginPortal()
-    maven("https://hypertrace.jfrog.io/artifactory/maven")
+    maven("https://us-maven.pkg.dev/hypertrace-repos/maven")
   }
 }
 
 plugins {
-  id("org.hypertrace.version-settings") version "0.2.1"
+  id("org.hypertrace.version-settings") version "0.3.0"
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
